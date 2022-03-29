f80860b160f66a74db72709d6103d1e06514da84
Bruno Oliveira <brunosoe@motorola.com>
2022-03-29 00:11:33 -0300

    feat: map first char to a house

--- src/services/UserInforProvider.js
+++ src/services/UserInforProvider.js
@@ -1,5 +1,7 @@
-const getHouse = () => {
-  return 'slytherin';
+const getHouse = (name) => {
+  let index = name[0].toUpperCase().charCodeAt(0)%4;
+  let houses = ['ravenclaw', 'hufflepuff', 'gryffindor', 'slytherin'];
+  return houses[index];
 };
 
 export {getHouse};
