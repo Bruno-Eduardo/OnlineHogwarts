1e99b0b0a877ad7dd2ec7d9272e40bbd3c5c6dd1
Bruno Oliveira <brunosoe@motorola.com>
2022-03-28 22:13:05 -0300

    feat: background at Spell

--- src/screens/Spells.js
+++ src/screens/Spells.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, ImageBackground, StyleSheet, Text, View} from 'react-native';
 import HogwartsButton from '../components/HogwartsButton';
 
 export default class Spells extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Spells</Text>
-        <Text>Lista a ser implementada</Text>
-        <HogwartsButton
-          title="Return"
-          screen="Return"
-          navigation={this.props.navigation}
-        />
-      </View>
+      <ImageBackground
+        source={require('../assets/images/wand.jpg')}
+        resizeMode="cover"
+        style={{flex: 1, height: undefined, width: undefined}}>
+        <View style={styles.container}>
+          <Text>Spells</Text>
+          <Text>Lista a ser implementada</Text>
+          <HogwartsButton
+            title="Return"
+            screen="Return"
+            navigation={this.props.navigation}
+            UserProps={'I was at Spells, line 19'}
+          />
+        </View>
+      </ImageBackground>
     );
   }
 }
